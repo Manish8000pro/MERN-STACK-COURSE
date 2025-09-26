0da114627008654174a57a049cdced5e602d8442
Manish8000pro <manishkumawat6227@gmail.com>
2025-09-26 15:41:36 +0530

    day17 completed

--- JS/Day02/17/index.js
+++ JS/Day02/17/index.js
@@ -37,16 +37,19 @@
 // let a = 7
 // let b = 4.5
 // console.log(a,b)
+// console.log(typeof a)
 
 // String 
 
 // let c = "Becoming a Pro.Coder"
 // let d = 'Manish'
+// console.log(typeof d)
 // console.log(c,d)
 
 // boolean
 
 // let login = true
+// console.log(typeof login)
 // let f = false
 // console.log(login,f)
 
@@ -65,31 +68,64 @@
 // Null
 
 // let weather = null
-
+// console.log(typeof null)
 // console.log(weather)
 
 // SymboS
 
-const id1 = Symbol("id")
+// const id1 = Symbol("id")
+// console.log(typeof id1)
 
-const id2 = Symbol("id")
+// const id2 = Symbol("id")
 
-console.log(id2==id1)
+// console.log(id2==id1)
 
 // Non Primitive data
 
 // Array , Object, function
 
-let arr = [10,29,11,"Manish",true]
-console.log(arr)
+// let arr = [10,29,11,"Manish",true]
+// console.log(typeof arr)
+// console.log(arr)
 
 // Object 
 // Manish  , 12422, gen
 
-let user = {
-    name:"Manish",
-    account: 12422,
-    categery:'OBC',
-    age:22, 
+// let user = {
+//     name:"Manish",
+//     account: 12422,
+//     categery:'OBC   ',
+//     age:22, 
+// }
+// console.log(typeof user)
+// console.log(user)
+// console.log(typeof user)
+// function add(){
+//     console.log("Hello")
+// }
+// add();
+
+// let s = function add(){
+//     console.log("Hello")
+// }
+// console.log(s);
+// s();
+
+// Primitive data type are immutble
+
+// let str = "Rohit"
+// console.log(str[0])
+
+// Non primitive data type are mutable
+
+let arr = [10, 20,30, 40,50]
+arr.push(90)
+arr[0] = 70
+console.log(arr)
+
+let obj = {
+    name: "Madav",
+    age: 22
 }
-console.log(user)
+obj.name = "Mohan"
+console.log(obj)
