a18fcf994223f53af77a86d071803f422d34f155
Manish8000pro <manishkumawat6227@gmail.com>
2025-09-26 15:01:39 +0530

    day17

---
+++ JS/Day02/17/index.js
@@ -0,0 +1,95 @@
+// variable ko kese bnate h 
+
+
+// let name = "Manish"
+// let age = 22
+
+// console.log(name,age)
+
+// const account = 1234;
+// account= 4
+// console.log(account)
+
+// Old method 
+
+// var a =20 
+// var a = 30
+
+// console.log(a)
+
+// if (true) {
+//     var a = 20
+// }
+
+// function fun(){
+//     var c = 40
+// }
+// var b=30
+// console.log(c)
+
+// Data Types 
+// Primitives data types
+
+// Number, String,boolean,undefined,null,bigint,symbol
+
+// Number
+
+// let a = 7
+// let b = 4.5
+// console.log(a,b)
+
+// String 
+
+// let c = "Becoming a Pro.Coder"
+// let d = 'Manish'
+// console.log(c,d)
+
+// boolean
+
+// let login = true
+// let f = false
+// console.log(login,f)
+
+// Undefined 
+
+// let user;
+
+// console.log(user);
+
+//bigint 
+
+// let num = 3467543334564433n
+
+// console.log(num)
+
+// Null
+
+// let weather = null
+
+// console.log(weather)
+
+// SymboS
+
+const id1 = Symbol("id")
+
+const id2 = Symbol("id")
+
+console.log(id2==id1)
+
+// Non Primitive data
+
+// Array , Object, function
+
+let arr = [10,29,11,"Manish",true]
+console.log(arr)
+
+// Object 
+// Manish  , 12422, gen
+
+let user = {
+    name:"Manish",
+    account: 12422,
+    categery:'OBC',
+    age:22, 
+}
+console.log(user)
