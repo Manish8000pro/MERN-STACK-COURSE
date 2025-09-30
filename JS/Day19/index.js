70dd726a5a45b6919620ff5add74f4cc64d60b0a
Manish8000pro <manishkumawat6227@gmail.com>
2025-09-30 17:07:10 +0530

    Day19 completed

---
+++ JS/Day19/index.js
@@ -0,0 +1,201 @@
+// Operators
+ 
+// Arithmetic Operator
+
+// console.log(2+4)
+// console.log(4-2)
+// console.log(2*4)
+// console.log(4/2)
+// for reminder
+// console.log(4%2)
+// this is the 4*4
+// console.log(4**2) 
+
+// Assignment Operator
+
+// let a = 20
+// let b = 10
+// c = (a-b)
+// c = (a+b)
+// c = (a*b)
+// c = (a/b)
+// console.log(c)
+
+// let a = 4
+// let b = 2
+// c = (a**b)
+// console.log(c)
+
+// Comparision Operator 
+
+let x = 20 
+let y = "20" 
+// console.log(x>y)
+// console.log(x<y)
+// console.log(x<=y)
+// console.log(x>=y)
+// console.log(x==y)
+
+// let z = "120"
+// let a = Number(z) 
+// console.log( a)
+
+// String --> Number
+// Number and strin compare (string conver in Number)
+// console.log(x==y)
+
+// let a = "121ac"
+// let b = Number(a)
+// console.log(typeof b)   //Nan is Not A number
+
+// String --> Number
+
+// let a = 10 
+// let b = String(a)
+// console.log(typeof b)
+
+// Boolean --> Number
+
+// console.log(Number(true))
+// console.log(Number(false))
+
+// Null --> Number
+// console.log(Number(null))
+// console.log(Number(undefined))
+
+// console.log(typeof String(undefined))
+
+// Boolean convert
+// console.log(Boolean(-20))
+// console.log(Boolean(0))
+
+// computer science challenge
+
+// let a = "0.1"
+// let b = "0.2"
+
+
+// console.log(c)
+
+// Null is losely equal to undefined
+
+// console.log(null == undefined)
+// console.log(null == true)
+// console.log(null == false)
+// console.log(null == "")
+// console.log(null == 0)
+
+// <,>,<=,>= (Null --> Number, undefined --> NaN)
+
+// console.log(null<=0)
+// console.log(null>=0)
+// console.log(null<0)
+// console.log(null>0)
+// console.log(null>=undefined)
+// console.log(undefined>=0)
+
+// first Elphabet Check
+// console.log("Manish">"Dinesh")
+
+// console.log(10<true)
+
+// console.log(null>="")
+
+
+// console.log(NaN == NaN)
+
+// Loop kese kaam krte h
+
+// for Loop 
+
+// for(let i = 1; i<=10; i++){
+//     console.log(i)
+// }
+
+// while Loop
+
+// let i = 1;
+
+// while(i<=10) {
+    
+//     console.log(i)
+//     i++;
+// }
+
+// Do while loop 
+
+
+// let i = 1;
+
+// do{console.log(i)
+//     i++;
+// }while(i<=10)
+
+// if else 
+
+// let age = 25;
+
+// if (age>=18){
+//     console.log("Elegible")
+// }
+// else{
+//     console.log("Not Elegible")
+// }
+
+// else if loop
+
+// let age = 60;
+
+// if(age<=18){
+//     console.log("Kid")
+// }
+// else if(age>=60){
+//     console.log("Old")
+// }
+// else{
+//     console.log("Young")
+// }
+
+// Logical Operator 
+
+// and operator &&
+
+// console.log(true&&true)
+// console.log(true&&false)
+// console.log(false&&false)
+// console.log(false&&true)
+
+// or Operator ||
+
+// console.log(true||true)
+// console.log(true||false)
+// console.log(false||false)
+// console.log(false||true)
+
+// let a = "Manish"
+// let b = "Dinesh"
+// let c = a&&b
+// console.log(c)
+
+// let a = 0;
+// let b = 9;
+
+// && : if first value is false ,it will be return first value itself 
+// if first value is true it will be return second value
+// console.log(a&&b)
+
+// Or ||
+
+// let a = 20 
+// let b = 10 
+
+// console.log(a||b)
+
+// || : if first value is false ,it will be return second  value 
+// if first value is true it will be return first  value
+
+// != 
+
+let a = 5 
+let b =5 
+console.log(a!=b)
