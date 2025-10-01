9069f0b59a24e1d6b72dd84962c89dcd9ac5ff41
Manish8000pro <manishkumawat6227@gmail.com>
2025-10-01 14:24:05 +0530

    Day20 of web.dev

---
+++ JS/Day20/5/index.js
@@ -0,0 +1,49 @@
+// Number 
+
+// let a = 20
+// let b = 345.6674
+
+// console.log(b.toFixed(1))
+// console.log(b.toPrecision(3))
+
+// let a = new Number(20)
+// let b = new Number(20)
+// console.log(a==b)
+
+// let obj1 = {
+//     name:"Manish"
+// }
+// obj2 = {
+//     name:"Manish"
+// }
+// let obj2 = obj1
+
+// console.log(obj1 == obj2)
+
+//Non Primitive : Copy by Reference 
+//Primitive : copy by value
+
+// console.log(Math.LN10)
+// console.log(Math.E)
+// console.log(Math.abs(-4))
+// console.log(Math.ceil(22.4))
+// console.log(Math.floor(22.4))
+// console.log(Math.SQRT2)
+// console.log(Math.max(23, 45, 22, 58, 78))
+// console.log(Math.min(23, 45, 22, 58, 78))
+
+
+//  (0-9)
+// console.log(Math.floor(Math.random()*10))
+
+// generate the value (1-10)
+
+// console.log(Math.floor(Math.random()*10)+1)
+
+// (15-35) generate the number 
+
+// console.log(Math.floor(Math.random()*21)+15)
+
+// OTP Generate :4 digit : 1000-9999
+
+console.log(Math.floor(Math.random()*(9999-1000)+1000))
