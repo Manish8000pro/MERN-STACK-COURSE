35664e67ff9964f7a40a46a6bde1410ce12e3dd3
Manish8000pro <manishkumawat6227@gmail.com>
2025-10-02 17:09:47 +0530

    day21 string completed

---
+++ JS/Day21/6/index.js
@@ -0,0 +1,56 @@
+// String 
+
+// const str1 = "Manish"
+// const str2 = 'Dinesh'
+// const day = 18 
+// const str3 = `Strike is comming on ${day}`
+
+// console.log(str1,str2,str3)
+// console.log(str3)
+
+// const str = `hello coder army`
+// console.log(str.length)
+// console.log(str[6])
+
+// // UPPER CASE
+
+
+// console.log(str.toUpperCase())
+
+// // Lower case 
+
+// console.log(str.toLowerCase())
+
+// const str = `Hello coder Army coder`
+// console.log(str.indexOf('cod'))
+// console.log(str.lastIndexOf('cod'))
+// console.log(str.includes('Cod'))
+
+
+// slice
+// console.log(str.slice(3,9))
+// console.log(str.slice(6))
+// console.log(str.slice(6))
+// console.log(str.slice(-6,-2))
+
+// const str = `Hello coder Army coder`
+
+// console.log(str.substring(2,8))
+
+// const a = "Manish"
+// const b = " Kumawat"
+// const c = a+b;
+// console.log(c)
+
+
+// const str = `Hello coder Army coder`
+// console.log(str.replace('er', 'ing'))
+
+// const user = " Manish Kumawat "
+// console.log(user.trim())
+
+// const names = "Manish,Dinesh,Mamta, suraj, bhanu"
+// console.log(names.split(","))
+
+const now = new Date();
+console.log(now.toString())
