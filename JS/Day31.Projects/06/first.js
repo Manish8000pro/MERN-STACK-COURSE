67da7629950a05fe39ea6b280fc1fc4bb092883a
Manish8000pro <manishkumawat6227@gmail.com>
2025-10-23 22:38:39 +0530

    incomplete

--- JS/Day31.Projects/06/first.js
+++ JS/Day31.Projects/06/first.js
@@ -1,21 +1,28 @@
-const result = document.getElementById("Parent")
-const currentTime = Date.now()
+setInterval(()=>{
+    const result = document.getElementById("Parent")
+    const currentTime = Date.now()
 
-const OlympiceTime = new Date(2028,6,14) .getTime();
-let timer = OlympiceTime-currentTime;
+    const OlympiceTime = new Date(2028,6,14).getTime();
+    // console.log(OlympiceTime)
+    let timer = OlympiceTime-currentTime;
 
-// millisecond 
 
-const day = Math.floor((timer)/(1000*60*60*24))
-timer%= 1000*60*60*24;
+    // millisecond 
 
-const hour = Math.floor((timer)/(1000*60*60))
-timer%= 1000*60*60;
+    const day = Math.floor((timer)/(1000*60*60*24))
+    timer%=1000*60*60*24;
 
-const minute = Math.floor((timer)/(1000*60))
-timer%= 1000*60;
+    const hour = Math.floor((timer)/(1000*60*60))
+    timer%=1000*60*60;
 
-const see = Math.floor((timer)/(1000))
-timer%= 1000;
+    const minute = Math.floor((timer)/(1000*60))
+    timer%=1000*60;
 
-result.textContent = `${day}: Days ${hour}:Hours ${minute}:Minutes ${see}:Seconds`
+    const sec = Math.floor((timer)/(1000))
+    timer%=1000;
+
+
+    result.textContent = `${day}:Days ${hour}:Hours ${minute}:Minutes ${sec}:Seconds`;
+},1000)
+
+
