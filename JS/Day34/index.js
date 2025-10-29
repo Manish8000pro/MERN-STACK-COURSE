a410a1e4ac24dc4d9ce7f0122297e361b4b59fde
Manish8000pro <manishkumawat6227@gmail.com>
2025-10-29 17:20:04 +0530

    promises

--- JS/Day34/index.js
+++ JS/Day34/index.js
@@ -1,9 +1,40 @@
-console.log("Hello World")
+// console.log("Hello World")
 
 // code
 
+// const j1 = {
+//     name:"Manish",
+//     age:22,
+//     address: "Rajasthan",
+// }
+
+// convert to json
+// const jsonFormat = JSON.stringify(j1);
+// console.log(jsonFormat);
+
+// const jsonFormat = `{
+//     "name": "manish",
+//     "age": "22",
+//     "address": "Palsana"
+// }`;
+
+// Java Script Object
+// const JsObject = JSON.parse(jsonFormat);
+// console.log(JsObject);
 
 
 
+// console.log("ThankYou")
 
-console.log("ThankYou")
+
+// const p1 = new Promise((resolve,reject)=>{
+//     resolve({
+//         name: "Manish",
+//         age: "22"
+//     });
+// })
+// p1.then((response)=>{
+//     console.log(response);
+// }).catch((error)=>{
+//     console.log(error);
+// })
