441981a61d97f91537bbffc3c66efb2a78b5234b
Manish8000pro <manishkumawat6227@gmail.com>
2025-11-11 14:14:23 +0530

    day 35 ongoing

---
+++ JS/Day35/index.js
@@ -0,0 +1,49 @@
+// async await Function 
+// async Function always return a Promise
+// async function greet(){
+//     // return "Manish";
+//     return new Promise((resolve,reject)=>{
+//         reject("Manish")
+//     })
+// }
+// const response = greet();
+// // console.log(response)
+// response.then((data)=>console.log(data))
+// .catch((error)=>{
+//     console.log("Error:",error);
+// })
+
+const { createElement } = require("react");
+
+// fetch("https://api.github.com/users")
+// .then((response)=>response.json())
+// .then((data)=>console.log(data));
+
+async function github() {
+    const response = await fetch("https://api.github.com/users")
+    const data = await response.json();
+    // console.log(data);
+
+    for(let user of data){
+       const element =  document.createElement("div")
+       element.classList.add("user")
+
+       const image = document.createElement('img');
+       image.src = user.avatar_url;
+
+       const userName = document.createElement('h2')
+       userName.textContent = user.login;
+
+       const anchor = document.createElement('a')
+       anchor.href = user.html_url;
+       anchor.textContent = "Visit Profile"
+
+       element.append(image,userName,anchor);
+
+       parent.append(element)
+    }
+}
+
+github();
+
+// console.log("Hello ji Kese Ho")
