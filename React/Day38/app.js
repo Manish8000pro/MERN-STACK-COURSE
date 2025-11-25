f101a693dd3557d16636525183fcbe3791dedd1e
Manish8000pro <manishkumawat6227@gmail.com>
2025-11-25 20:35:39 +0530

    react

--- React/Day38/app.js
+++ React/Day38/app.js
@@ -5,6 +5,9 @@ const element1 = document.createElement('h1');
 element1.textContent = "Hello Coder Army";
 element1.className = 'element'
 element1.id = 'first';
+element1.style.fontSize="30px";
+element1.style.backgroundColor = "orange";
+element1.style.color = "white";
 
 const root = document.getElementById('root');
 root.append(element1);
