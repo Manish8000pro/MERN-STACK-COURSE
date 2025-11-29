030fd105858b3b373e2ee06466e50213246c6c7e
Manish8000pro <manishkumawat6227@gmail.com>
2025-11-29 17:19:50 +0530

    class 3 of react installation of all npm

--- React/Day40 and Day2/app.js
+++
@@ -1,4 +0,0 @@
-
-const element = <h1>Hello Coders Kese ho App </h1>
-
-ReactDOM.createRoot(document.getElementById('root')).render(element);
---
+++ React/Day40 and Day3/app.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const element = <h1>Hello Coders Kese ho App </h1>;
+
+ReactDOM.createRoot(document.getElementById('root')).render(element);
