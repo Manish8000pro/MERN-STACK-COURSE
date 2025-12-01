6bb21ddc61e7df3ea06c584eaaf79a978e175724
Manish8000pro <manishkumawat6227@gmail.com>
2025-12-01 23:16:27 +0530

    react day4

---
+++ React/Day4.1 and 41/vite-project/src/App.jsx
@@ -0,0 +1,30 @@
+
+import { useState } from "react";
+
+function App(){
+
+  let [count,setCount] = useState(0);
+
+  // let count = 0;
+
+  // function increaseNumber(){
+  //   count++;
+    // console.log(count)
+    // const para = document.querySelector('p')
+    // para.textContent = `Counter:${count}`;
+  
+
+    function increaseNumber(){
+    count++;
+    setCount(count);
+  }
+
+  return (
+    <>
+    <p>Counter:{count}</p>
+    <button onClick={increaseNumber}>Increment:{count}</button>
+    </>
+  )
+}
+
+export default App;
