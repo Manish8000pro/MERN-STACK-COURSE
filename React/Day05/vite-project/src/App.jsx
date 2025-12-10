a589aeb4360489b8097ab63007e9a045518a5cc0
Manish8000pro <manishkumawat6227@gmail.com>
2025-12-10 17:32:28 +0530

    displayed the users profile and add the input box

--- React/Day05/vite-project/src/App.jsx
+++ React/Day05/vite-project/src/App.jsx
@@ -1,24 +1,38 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App(){
 
   const [Users,setUsers]=useState([])
+  const [name,setName] = useState("")
 
-  async function GithubProfile() {
+  useEffect(()=>{
+
+
+    async function GithubProfile() {
     const response = await fetch("https://api.github.com/users")
     const data = await response.json();
     setUsers(data);
+    console.log("Hello")
   }
 
-  GithubProfile();
+    GithubProfile();
+    
+  },[])
+
+  function handleChange(e){
+    setName(e.target.value.toUpperCase());
+  }
+
+  
 
   return(
     <>
     <h1>Github Users </h1>
+    <input type="text" value={name} onChange={handleChange} />
     <div style={{display:"flex",justifyContent:"center",alignItems:"center",flexWrap:"wrap",gap:"10px"}}>
     {
       Users.map(Users=>(
-        <img src = {Users.avatar_url} height={"100px"} width={"100px"} />
+        <img src = {Users.avatar_url} height={"100px"} width={"100px"} key={Users.login} />
       ))
     }
     </div>
