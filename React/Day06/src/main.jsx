4c7e6f954def82443ce4fa3595c5692f622c5f31
Manish8000pro <manishkumawat6227@gmail.com>
2025-12-30 17:23:44 +0530

    Clock and counter app

--- React/Day06/src/main.jsx
+++ React/Day06/src/main.jsx
@@ -1,11 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import List from './List.jsx'
+import List from './List.jsx';
+import Clock from './Clock.jsx';
+
 
 
 createRoot(document.getElementById('root')).render(
   
     // <App />
-    <List></List>
+    <Clock></Clock>
 )
