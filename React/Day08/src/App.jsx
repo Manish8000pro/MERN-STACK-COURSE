6b581261b57e05d9e00ccd92a03c235484ae194e
Manish8000pro <manishkumawat6227@gmail.com>
2026-01-02 09:50:00 +0530

    Stopwatch created

---
+++ React/Day08/src/App.jsx
@@ -0,0 +1,41 @@
+import { useRef, useState } from 'react'
+
+
+function App() {
+  const [time, setTime] = useState(0);
+  const intervalIdRef = useRef(null);
+
+  function handleStart(){
+
+    if(intervalIdRef.current!=null){
+      return;
+    }
+
+
+    intervalIdRef.current = setInterval(()=>{
+      setTime(time=>time+1);
+    },1000);
+  }
+
+  function handleStop(){
+    clearInterval(intervalIdRef.current);
+    intervalIdRef.current = null;
+  }
+
+  function handleReset(){
+    clearInterval(intervalIdRef.current);
+    intervalIdRef.current = null;
+    setTime(0);
+  }
+
+  return (
+    <>
+      <h1>Stopwatch:{time}</h1>
+      <button onClick={handleStart}>Start</button>
+      <button onClick={handleStop}>Stop</button>
+      <button onClick={handleReset}>Reset</button>
+    </>
+  )
+}
+
+export default App
