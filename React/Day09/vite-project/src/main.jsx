4ecb70fc2a070622b0c02557765cd368ec828f17
Manish8000pro <manishkumawat6227@gmail.com>
2026-01-18 23:46:08 +0530

    facing the problem

--- React/Day09/vite-project/src/main.jsx
+++ React/Day09/vite-project/src/main.jsx
@@ -1,9 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
+// import App from './App'
+import Form from './Form'
 
 createRoot(document.getElementById('root')).render(
   
-    <App />
+    // <App />
+    <Form></Form>
   
 )
