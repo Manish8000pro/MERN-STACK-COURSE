029e663c3ecdb46b6950878da1b704e6c0f3b3e7
Manish8000pro <manishkumawat6227@gmail.com>
2026-01-18 22:39:45 +0530

    Reach hooks

--- React/Day09/vite-project/src/App.jsx
+++ React/Day09/vite-project/src/App.jsx
@@ -1,35 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name,setName] = useState('');
+  const [age,setAge] = useState('');
+  const [password,setPassword] = useState('');
 
-  return (
+
+  function handleSubmit(e){
+    e.preventDefault();
+    console.log(name);
+    console.log(age);
+    console.log(password);
+  }
+
+  return(
     <>
+    <form onSubmit={handleSubmit}>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input type="text" value={name} onChange={(e)=>setName(e.target.value)} />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div>
+        <input type="number" value={age} onChange={(e)=>setAge(e.target.value)} />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div>
+        <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} />
+      </div>
+      <button>Submit</button>
+    </form>
     </>
   )
 }
 
-export default App
+export default App;
