9c0179a391c43c784eff9a569aa22684abf2719b
Manish8000pro <manishkumawat6227@gmail.com>
2026-05-20 22:37:10 +0530

    updating

--- CSS/Day15/script.js
+++ CSS/Day15/script.js
@@ -125,3 +125,34 @@ counters.forEach(counter => {
     updateCounter();
 
 });
+
+
+// Active navigation higilight
+const sections = document.querySelectorAll("section, div[id]");
+const navLinks = document.querySelectorAll("nav a");
+
+window.addEventListener("scroll",()=>{
+
+    let current = "";
+
+    sections.forEach(section=>{
+
+        const sectionTop = section.offsetTop;
+
+        if(pageYOffset >= sectionTop - 200){
+            current = section.getAttribute("id");
+        }
+
+    });
+
+    navLinks.forEach(link=>{
+
+        link.classList.remove("active");
+
+        if(link.getAttribute("href") === `#${current}`){
+            link.classList.add("active");
+        }
+
+    });
+
+});
