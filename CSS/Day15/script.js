4d9bd3a171137d610aaaaeace1352f9e9f223df7
Manish8000pro <manishkumawat6227@gmail.com>
2026-05-20 22:32:03 +0530

    Js update

--- CSS/Day15/script.js
+++ CSS/Day15/script.js
@@ -17,3 +17,111 @@ themeToggle.addEventListener("click",()=>{
     }
 
 });
+
+window.addEventListener("scroll",()=>{
+
+    const scrollTop = document.documentElement.scrollTop;
+
+    const height =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+
+    const scrollPercent = (scrollTop / height) * 100;
+
+    document.getElementById("progress-bar").style.width =
+    scrollPercent + "%";
+
+});
+
+// Scroll Reveal Animation
+
+const hiddenElements = document.querySelectorAll(
+".project, .item, .stats-card, .edfirst, .edsecond"
+);
+
+const observer = new IntersectionObserver((entries)=>{
+
+    entries.forEach((entry)=>{
+
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+
+    });
+
+});
+
+hiddenElements.forEach((el)=>{
+
+    el.classList.add("hidden");
+
+    observer.observe(el);
+
+});
+
+// Typing Animation
+const text = [
+    "Frontend Developer",
+    "AI Enthusiast",
+    "JavaScript Learner",
+    "Creative Coder",
+    "Data Structure and Algorithm"
+];
+
+let count = 0;
+let index = 0;
+let currentText = "";
+let letter = "";
+
+(function type(){
+
+    if(count === text.length){
+        count = 0;
+    }
+
+    currentText = text[count];
+    letter = currentText.slice(0, ++index);
+
+    document.getElementById("typing-text").textContent = letter;
+
+    if(letter.length === currentText.length){
+        count++;
+        index = 0;
+
+        setTimeout(type,1000);
+    }else{
+        setTimeout(type,100);
+    }
+
+})();
+
+// Status section  Animated Status section
+const counters = document.querySelectorAll(".counter");
+
+counters.forEach(counter => {
+
+    counter.innerText = "0";
+
+    const updateCounter = () => {
+
+        const target = +counter.getAttribute("data-target");
+
+        const c = +counter.innerText;
+
+        const increment = target / 100;
+
+        if(c < target){
+
+            counter.innerText = `${Math.ceil(c + increment)}`;
+
+            setTimeout(updateCounter,30);
+
+        }else{
+            counter.innerText = target;
+        }
+
+    };
+
+    updateCounter();
+
+});
