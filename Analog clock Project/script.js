ed6cf0aafc67fa60b4a995bc32a4488bdc40693c
Manish8000pro <manishkumawat6227@gmail.com>
2026-06-29 22:26:13 +0530

    Javascript file of clock

---
+++ Analog clock Project/script.js
@@ -0,0 +1,18 @@
+function clock(){
+    let hours = document.querySelector(".hours");
+    let minute = document.querySelector(".minute");
+    let second = document.querySelector(".second");
+
+    let date = new Date();
+    // console.log(date)
+    let hr = date.getHours();
+    let mint = date.getMinutes();
+    let sec = date.getSeconds();
+
+    hours.style.transform = `rotateZ(${hr*30 + mint/12}deg)`;
+    minute.style.transform = `rotateZ(${mint*6}deg)`;
+    second.style.transform = `rotateZ(${sec*6}deg)`;
+
+}
+clock();
+setInterval(clock,1000);
