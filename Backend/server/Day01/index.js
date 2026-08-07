a454f422bd8830e5075252d0cc853b0b64b1409d
Manish8000pro <manishkumawat6227@gmail.com>
2026-08-07 23:40:58 +0530

    Day 01

---
+++ Backend/server/Day01/index.js
@@ -0,0 +1,19 @@
+const http = require("http");
+const fs = require("fs");
+
+const myServer = http.createServer((req,res)=>{
+    const log = `${Date.now()}: ${req.url} New Req Recived\n`;
+    fs.appendFile('log.txt',log,(err,data)=>{
+        switch(req.url){
+            case '/': res.end("Home Page ");
+            break
+            case '/about' : res.end("I am Manish kumawat");
+            break
+            default:res.end("404 Not Found");
+        }
+        
+    })
+    
+});
+
+myServer.listen(8000,()=>console.log("Server started"));
